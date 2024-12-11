c33935b0137e7bba152a7e8558a18a607b1b9468
nagyum <wildanimal19@naver.com>
2024-12-11 19:18:49 +0900

    사이트 통합 완

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./styles/color.scss";
-import "./styles/font.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
