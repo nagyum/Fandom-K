5ca4009b273242a78161a7a4a019f3fe380b46fe
songhyungjin <sori4606@naver.com>
2024-12-16 17:11:24 +0900

    클릭아이콘 넣기

--- src/components/IdolCard/IdolCard.jsx
+++ src/components/IdolCard/IdolCard.jsx
@@ -1,7 +1,16 @@
 import styles from "./IdolCard.module.scss";
 import deleteIcon from "../../assets/icons/deleteIcon.png";
+import clickIcon from "../../assets/icons/clickIcon.png";
 
-function IdolCard({ imageUrl, name, group, isbig = true, onDelete }) {
+function IdolCard({
+  imageUrl,
+  name,
+  group,
+  isbig = true,
+  onDelete, // x표시 있는 사진
+  isClicked, //사진 선택되었는지
+  onClick,
+}) {
   return (
     <div
       className={`${styles.add_idol__container} ${
@@ -10,12 +19,27 @@ function IdolCard({ imageUrl, name, group, isbig = true, onDelete }) {
     >
       {" "}
       {/** isbig이 true면 프로필 크기가 128*183, false면 98*150 */}
-      <img
-        src={imageUrl}
-        className={`${styles.add_idol_image} ${
-          isbig ? styles.big : styles.small
-        }`}
-      />
+      {/** isClicked가 true면 클릭된 핑크색 배경 이미지, false면 그대로*/}
+      {isClicked ? (
+        <div className={styles.add_idol_image_container}>
+          <img src={clickIcon} className={styles.click_icon} />
+          <img
+            src={imageUrl}
+            className={`${styles.add_idol_image_clicked} ${
+              isbig ? styles.big : styles.small
+            }`}
+            onClick={onClick}
+          />
+        </div>
+      ) : (
+        <img
+          src={imageUrl}
+          className={`${styles.add_idol_image} ${
+            isbig ? styles.big : styles.small
+          }`}
+          onClick={onClick}
+        />
+      )}
       {/** onDelete prop 받을때만 X 아이콘 뜨게 */}
       {onDelete && (
         <img
