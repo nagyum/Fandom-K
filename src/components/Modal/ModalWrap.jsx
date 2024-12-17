3d5e92f34e439785919d62e136045f3dcbff92c8
김은경 <kng4316@gmail.com>
2024-12-17 14:56:41 +0900

    chore: 모달 팝업 애니메이션 추가

--- src/components/Modal/ModalWrap.jsx
+++ src/components/Modal/ModalWrap.jsx
@@ -1,14 +1,14 @@
 import styles from "./ModalWrap.module.scss";
 import deleteBtn from "../../assets/icons/btn_delete.png";
 
-function ModalWrap({ children, handleDeleteModal }) {
+function ModalWrap({ children, handleDeleteModal, style }) {
   //X버튼 혹은 뒷배경 클릭시 팝업창 닫기
   const onclickDeleteBtn = () => {
     handleDeleteModal();
   };
 
   return (
-    <div>
+    <div className={styles.modal_wrap} style={style}>
       <div className={styles.modal}>
         <img
           className={styles.modal_deleteBtn}
