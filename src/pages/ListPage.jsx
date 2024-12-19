7c8f501b27deb211e19aed20cc5ad05302d28381
nagyum <wildanimal19@naver.com>
2024-12-19 09:43:12 +0900

    chore. 투표하기 미디어쿼리 완

--- src/pages/ListPage.jsx
+++ src/pages/ListPage.jsx
@@ -6,32 +6,26 @@ import SponsorshipList from "../components/SponsorshipList/SponsorshipList";
 import useScrollTop from "../hooks/useScrollTop";
 import SponsorshipModal from "../components/SponsorshipList/SponsorshipModal";
 import ModalWrap from "../components/Modal/ModalWrap";
+import MediaModalWrap from "../components/Modal/MediaModalwrap";
 import VoteModal from "../components/MonthList/components/VoteModal";
-
 import ChargeCreditModal from "../components/Modal/ChargeCreditModal";
-
-
 import Footer from "../components/Footer/Footer";
-
+import useDevice from "../hooks/useDevice"; // 미디어 쿼리
 
 function ListPage() {
-  const [modalContents, setModalContents] = useState(); // 1,2,3,4
-  //모달이 열렸는지
-  const [isModal, setIsModal] = useState(false);
-  //opacity로 modal 애니메이션주기
-  const [modalOpacity, setModalOpacity] = useState(100);
-  //데이터 상태관리
+  const [modalContents, setModalContents] = useState(); // 모달 타입
+  const [isModal, setIsModal] = useState(false); // 모달 열림 여부
+  const [modalOpacity, setModalOpacity] = useState(100); // 모달 투명도
   const [sponsorData, setSponsorData] = useState();
   const [voteData, setVoteData] = useState();
-
   const [myCreditData, setMyCreditData] = useState();
-
   const [pageSize, setPageSize] = useState(10);
   const [gender, setGender] = useState("female");
 
-  useScrollTop();
+  const { mode } = useDevice(); // 모바일/데스크톱 감지
+  useScrollTop(); // 스크롤 초기화
 
-  // 모달 상태에 따라 배경 스크롤 비활성화
+  // 모달이 열릴 때 배경 스크롤 비활성화
   useEffect(() => {
     if (isModal) {
       document.body.style.overflow = "hidden";
@@ -43,7 +37,7 @@ function ListPage() {
     };
   }, [isModal]);
 
-  //후원 모달 팝업 띄우기
+  // 후원 모달 열기
   const handleSponsorModal = (data) => {
     setIsModal(true);
     setSponsorData(data);
@@ -53,7 +47,7 @@ function ListPage() {
     }, 0);
   };
 
-  //투표 모달 팝업 띄우기
+  // 투표 모달 열기
   const handleVoteModal = (data) => {
     setIsModal(true);
     setVoteData(data);
@@ -62,43 +56,36 @@ function ListPage() {
       setModalOpacity(100);
     }, 0);
   };
-  //크레딧 충전 모달 팝업 띄우기
+
+  // 크레딧 충전 모달 열기
   const handleMyCreditModal = (data) => {
     setIsModal(true);
     setMyCreditData(data);
     setModalContents(4);
   };
-  //충전 크레딧 업데이트
+
+  // 크레딧 충전
   const handleCharge = (amount) => {
-    setMyCreditData((prev)=> prev + amount);
+    setMyCreditData((prev) => prev + amount);
     setIsModal(false);
-  }
+  };
 
-
-
-
-
-  //모달 팝업 X버튼 클릭시 닫기
+  // 모달 닫기
   const handleDeleteModal = () => {
     setModalOpacity(0);
     setTimeout(() => {
       setIsModal(false);
-      setSponsorData();
-      setVoteData();
+      setSponsorData(null);
+      setVoteData(null);
     }, 200);
   };
 
-  //1번 후원하기, 2번 투표하기, 3번 크레딧 부족, 4번 크레딧 충전
+  // 모달 내용 선택
   function ModalContents({ modalContents }) {
     switch (modalContents) {
-      case 1: //후원하기
+      case 1:
         return <SponsorshipModal data={sponsorData} />;
-      case 2: //투표하기
-
-        return <VoteModal />;
-      case 4: //크레딧 충전
-        return <ChargeCreditModal onCharge={handleCharge} />;
-
+      case 2:
         return (
           <VoteModal
             data={voteData}
@@ -106,32 +93,43 @@ function ListPage() {
             gender={gender}
           />
         );
-
+      case 4:
+        return <ChargeCreditModal onCharge={handleCharge} />;
       default:
-        break;
+        return null;
     }
   }
 
   return (
     <div>
       <Header />
-
-      <MyCredit handleMyCreditModal={handleMyCreditModal} myCreditData={myCreditData} />
-
-      <MyCredit />
-
+      <MyCredit
+        handleMyCreditModal={handleMyCreditModal}
+        myCreditData={myCreditData}
+      />
       <SponsorshipList handleSponsorModal={handleSponsorModal} />
-      {isModal && (
-
-        <ModalWrap
-          style={{ opacity: `${modalOpacity}%` }}
-          handleDeleteModal={handleDeleteModal}
-        >
-          <div style={{ maxHeight: "80vh", overflowY: "auto" }}>
-            <ModalContents modalContents={modalContents} />
-          </div>
-        </ModalWrap>
-      )}
+      {isModal &&
+        (modalContents === 2 && mode === "mobile" ? (
+          // VoteModal을 모바일 환경에서 MediaModalWrap으로 감싸기
+          <MediaModalWrap
+            style={{ opacity: `${modalOpacity}%` }}
+            handleDeleteModal={handleDeleteModal}
+          >
+            <div>
+              <ModalContents modalContents={modalContents} />
+            </div>
+          </MediaModalWrap>
+        ) : (
+          // 다른 모든 모달은 ModalWrap 사용
+          <ModalWrap
+            style={{ opacity: `${modalOpacity}%` }}
+            handleDeleteModal={handleDeleteModal}
+          >
+            <div style={{ maxHeight: "80vh", overflowY: "auto" }}>
+              <ModalContents modalContents={modalContents} />
+            </div>
+          </ModalWrap>
+        ))}
       <MonthsList
         handleVoteModal={handleVoteModal}
         pageSize={pageSize}
