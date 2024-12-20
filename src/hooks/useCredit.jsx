ba209bb4efb6be154551a8cf35675e1c9ee1e9ca
유주섭 <noname@nonameui-MacBookAir.local>
2024-12-20 14:29:00 +0900

    로컬스토리지 연결 및 scss수정

--- src/hooks/useCredit.jsx
+++ src/hooks/useCredit.jsx
@@ -2,26 +2,49 @@ import {useEffect, useState} from "react";
 
 const useCredit = () => {
     const [credit, setCredit] = useState(
-        Number(localStorage.getItem("credit") || 0)
+      Number(localStorage.getItem("credit")) || 0
     );
-
+  
     const addCredit = (value) => {
-        setCredit((prev)=> prev+value);
+      setCredit((prev) => {
+        const newValue = prev + value; //입력한 값과 기존의 밸류를 더한 값
+        localStorage.setItem("credit", String(newValue)); 
+        window.dispatchEvent(new CustomEvent("creditChange")); //그래서 여기서 CustomEvent를 사용해서 다른컴포넌트들에 이 변경사실을 알림
+        return newValue;
+      });
     };
+  
     const subtractCredit = (value) => {
-        setCredit((prev) => prev-value);
+      setCredit((prev) => {
+        const newValue = prev - value;
+        localStorage.setItem("credit", String(newValue));
+        window.dispatchEvent(new CustomEvent("creditChange")); //여기도 마찬가지
+        return newValue;
+      });
     };
-    useEffect(()=>{
-        localStorage.setItem("credit", String(credit));
-    },[credit]);
+  
+    // 변경사항 감지
+    useEffect(() => {
+      const handleCreditChange = () => {
+        setCredit(Number(localStorage.getItem("credit")));
+      };
+  
+      window.addEventListener("creditChange", handleCreditChange); //여기서 creditChange 이벤트가 발생하면 handleCreditChange 실행시킴.
+  
+      return () => {
+        window.removeEventListener("creditChange", handleCreditChange);
+      };
+    }, []);
+  
+    return {
+      credit,
+      addCredit,
+      subtractCredit,
+    };
+  };
+  
 
-    
-    return{
-        credit,
-        addCredit,
-        subtractCredit,
-    };
-};
+  
 
 export default useCredit;
 
