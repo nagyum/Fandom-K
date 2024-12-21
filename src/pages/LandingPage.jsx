259bcd8b2e88758868142c87d1f3f3e60f49fe40
Kate Jang <bobaejang1@gmail.com>
2024-12-21 15:14:59 +0900

    애니메이션 적용

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -8,55 +8,84 @@ import images from "../components/LandingPage/LandingPageImages.jsx";
 import CustomButton from "../components/CustomButtom/CustomButton.jsx";
 import { useNavigate } from "react-router-dom";
 import EdgeGradient from "../components/LandingPage/EdgeGradient.jsx";
-import { useRef, useEffect, useState} from "react";
+import { motion } from "framer-motion";
 
 function LandingPage() {
   const navigate = useNavigate();
-  
-
 
   const handleButtonClick = () => {
     localStorage.clear(); // localStorage 초기화
     navigate("/list"); // 원하는 페이지로 이동
   };
 
-
-  
-
   return (
     <EdgeGradient>
       <div>
-        <div className={styles.page1}>
-          <div className={styles.background}>
-            <div className={styles.page1}>
-              <div className={styles.title}>
-                <h1>
-                  내가 좋아하는 아이돌을 <br />
-                  가장<span className={styles["brand-orange"]}> 쉽게 덕질</span> 하는
-                  방법
-                </h1>
-              </div>
-              <div className={styles["image-container"]}>
-                <img onClick={() => {
-                  navigate("/list");
-                }} className={styles.logo} src={logonobackground} alt="logo" />
-                <img className={styles.backgroundimg} src={images.bgimg1} alt="bgimg1"></img>
-              </div>
-              <div className={styles.buttondiv}>
-                <CustomButton
-                  onClick={handleButtonClick}
-                  className={styles.button}
-                  width={477}
-                  height={48}
-                >
-                  지금 시작하기
-                </CustomButton>
+        <motion.div
+          initial={{ opacity: 0, y: 50 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ once: false }}
+          transition={{
+            ease: "easeInOut",
+            duration: 2,
+            x: { duration: 1 },
+          }}
+        >
+          <div className={styles.page1}>
+            <div className={styles.background}>
+              <div className={styles.page1}>
+                <div className={styles.title}>
+                  <h1>
+                    내가 좋아하는 아이돌을 <br />
+                    가장
+                    <span className={styles["brand-orange"]}>
+                      {" "}
+                      쉽게 덕질
+                    </span>{" "}
+                    하는 방법
+                  </h1>
+                </div>
+                <div className={styles["image-container"]}>
+                  <img
+                    onClick={() => {
+                      navigate("/list");
+                    }}
+                    className={styles.logo}
+                    src={logonobackground}
+                    alt="logo"
+                  />
+                  <img
+                    className={styles.backgroundimg}
+                    src={images.bgimg1}
+                    alt="bgimg1"
+                  ></img>
+                </div>
+                <div className={styles.buttondiv}>
+                  <CustomButton
+                    onClick={handleButtonClick}
+                    className={styles.button}
+                    width={477}
+                    height={48}
+                  >
+                    지금 시작하기
+                  </CustomButton>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className={styles["gradient-bar"]}></div>
-        <div className={styles.page2}>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, y: 50 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ once: false }}
+          transition={{
+            ease: "easeInOut",
+            duration: 2,
+            x: { duration: 1 },
+          }}
+        >
+          <div className={styles.page2}>
+            <div className={styles["gradient-bar"]}></div>
             <Section
               donate="후원하기"
               title={
@@ -67,8 +96,19 @@ function LandingPage() {
               bgimg={images.bgimg2}
               screenimg={images.screen1}
             />
-        </div>
-        <div className={styles.page3}>
+          </div>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, y: 50 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ once: false }}
+          transition={{
+            ease: "easeInOut",
+            duration: 2,
+            x: { duration: 1 },
+          }}
+        >
+          <div className={styles.page3}>
             <Section
               donate="이달의 아티스트"
               title={
@@ -79,8 +119,19 @@ function LandingPage() {
               bgimg={images.bgimg3}
               screenimg={images.screen2}
             />
-        </div>
-        <div className={styles.page4}>
+          </div>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, y: 50 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ once: false }}
+          transition={{
+            ease: "easeInOut",
+            duration: 2,
+            x: { duration: 1 },
+          }}
+        >
+          <div className={styles.page4}>
             <Section
               donate="나만의 아티스트"
               title={
@@ -91,9 +142,9 @@ function LandingPage() {
               bgimg={images.bgimg4}
               screenimg={images.screen3}
             />
-        </div>
-
-        
+          </div>
+        </motion.div>
+        .
       </div>
     </EdgeGradient>
   );
